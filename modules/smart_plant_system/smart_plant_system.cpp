24972405271f632e52340d0cbbad2eddd4a36997
James Gaskell <james.gaskell@Jamess-MacBook-Pro-5.local>
2023-03-08 15:46:44 -0500

    setup the project files

---
+++ modules/smart_plant_system/smart_plant_system.cpp
@@ -0,0 +1,35 @@
+//=====[Libraries]=============================================================
+
+#include "arm_book_lib.h"
+
+#include "smart_plant_system.h"
+
+//=====[Declaration of private defines]========================================
+
+//=====[Declaration of private data types]=====================================
+
+//=====[Declaration and initialization of public global objects]===============
+
+//=====[Declaration of external public global variables]=======================
+
+//=====[Declaration and initialization of public global variables]=============
+
+//=====[Declaration and initialization of private global variables]============
+
+//=====[Declarations (prototypes) of private functions]========================
+
+//=====[Implementations of public functions]===================================
+
+void smartPlantSystemInit()
+{
+    //pcSerialComInit();
+}
+
+void smarPlantSystemUpdate()
+{
+    //pcSerialComUpdate();
+    //eventLogUpdate();
+    //delay(SYSTEM_TIME_INCREMENT_MS);
+}
+
+//=====[Implementations of private functions]==================================
